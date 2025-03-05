106c8a029fff7697fae5d30570d2efc1186ac6d4
mamiyusuf <erdilmuhammedyusuf1@gmail.com>
2025-03-05 09:51:45 +0300

    Add project files.

---
+++ odevtakipuygulaması/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace odevtakipuygulaması
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            listBox1 = new ListBox();
+            panel2 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            label8 = new Label();
+            textBox5 = new TextBox();
+            label9 = new Label();
+            textBox6 = new TextBox();
+            label10 = new Label();
+            textBox7 = new TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonHighlight;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(739, 79);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.White;
+            label1.Font = new Font("Segoe UI Semibold", 18F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.Teal;
+            label1.Location = new Point(227, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(344, 79);
+            label1.TabIndex = 1;
+            label1.Text = "ÖĞRENCİ TAKİP UYGULAMASI";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            label1.Click += label1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Left;
+            pictureBox1.Image = Properties.Resources.open_enrollment;
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(94, 79);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(0, 52);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(208, 484);
+            listBox1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(listBox1);
+            panel2.Location = new Point(0, 76);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(211, 533);
+            panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.DarkGreen;
+            label2.Location = new Point(-7, 13);
+            label2.Name = "label2";
+            label2.Size = new Size(215, 36);
+            label2.TabIndex = 3;
+            label2.Text = "ÖĞRENCİ LİSTESİ";
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(262, 155);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 23);
+            label3.TabIndex = 3;
+            label3.Text = "ADI";
+            // 
+            // label4
+            // 
+            label4.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(250, 224);
+            label4.Name = "label4";
+            label4.Size = new Size(81, 23);
+            label4.TabIndex = 3;
+            label4.Text = "SINIF";
+            label4.Click += label4_Click;
+            // 
+            // label5
+            // 
+            label5.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(262, 118);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 23);
+            label5.TabIndex = 3;
+            label5.Text = "NO";
+            // 
+            // label6
+            // 
+            label6.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(227, 191);
+            label6.Name = "label6";
+            label6.Size = new Size(81, 23);
+            label6.TabIndex = 3;
+            label6.Text = "SOYADI";
+            label6.Click += label4_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(323, 118);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(184, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(323, 155);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(184, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(323, 191);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(184, 23);
+            textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(323, 224);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(184, 23);
+            textBox4.TabIndex = 4;
+            // 
+            // label7
+            // 
+            label7.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(551, 102);
+            label7.Name = "label7";
+            label7.Size = new Size(162, 23);
+            label7.TabIndex = 3;
+            label7.Text = "ÖDEV YAPILDIMI";
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox2.Location = new Point(640, 155);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(86, 29);
+            checkBox2.TabIndex = 5;
+            checkBox2.Text = "HAYIR";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            checkBox1.Location = new Point(533, 155);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(73, 29);
+            checkBox1.TabIndex = 5;
+            checkBox1.Text = "EVET";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            label8.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(214, 279);
+            label8.Name = "label8";
+            label8.Size = new Size(152, 23);
+            label8.TabIndex = 3;
+            label8.Text = "ÖDEV KONUSU";
+            label8.Click += label4_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(372, 279);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(304, 82);
+            textBox5.TabIndex = 4;
+            // 
+            // label9
+            // 
+            label9.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(227, 374);
+            label9.Name = "label9";
+            label9.Size = new Size(152, 23);
+            label9.TabIndex = 3;
+            label9.Text = "ÖDEV POİNT";
+            label9.Click += label4_Click;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(361, 374);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(75, 23);
+            textBox6.TabIndex = 4;
+            // 
+            // label10
+            // 
+            label10.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label10.Location = new Point(284, 417);
+            label10.Name = "label10";
+            label10.Size = new Size(152, 23);
+            label10.TabIndex = 3;
+            label10.Text = "DERS";
+            label10.Click += label4_Click;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(361, 417);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(103, 23);
+            textBox7.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PaleGreen;
+            ClientSize = new Size(739, 604);
+            Controls.Add(checkBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(textBox5);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label4);
+            Controls.Add(label7);
+            Controls.Add(label5);
+            Controls.Add(label3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private PictureBox pictureBox1;
+        private ListBox listBox1;
+        private Panel panel2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Label label7;
+        private CheckBox checkBox2;
+        private CheckBox checkBox1;
+        private Label label8;
+        private TextBox textBox5;
+        private Label label9;
+        private TextBox textBox6;
+        private Label label10;
+        private TextBox textBox7;
+    }
+}
---
+++ odevtakipuygulaması/Form1.cs
@@ -0,0 +1,24 @@
+namespace odevtakipuygulaması
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
